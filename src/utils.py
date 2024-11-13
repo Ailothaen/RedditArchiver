fabb0349c1266624b311842c3ca9e07c8a515bf5
Ailothaen <mail@ailothaen.fr>
2024-11-13 18:06:01 +0100

    Considering new.reddit.com as valid URL

--- src/utils.py
+++ src/utils.py
@@ -7,8 +7,8 @@ def extract_id(url):
     Extracts the submission ID from a supplied URL
     """
     regexes = (r"^([a-z0-9]+)/?$",
-    r"^https?:\/\/(?:old|www)?\.reddit\.com\/([a-z0-9]+)\/?$",
-    r"^https?:\/\/(?:old|www)?\.reddit\.com\/r\/[a-zA-Z0-9\-_]+\/comments\/([a-z0-9]+)\/?")
+    r"^https?:\/\/(?:old|new|www)?\.reddit\.com\/([a-z0-9]+)\/?$",
+    r"^https?:\/\/(?:old|new|www)?\.reddit\.com\/r\/[a-zA-Z0-9\-_]+\/comments\/([a-z0-9]+)\/?")
 
     for regex in regexes:
         result = re.search(regex, url)
